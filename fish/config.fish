0173d47a638230854bc31823af076a5de412e7d5
Theo Oikonomou <thikonom@gmail.com>
2018-11-07 10:20:40 +0000

    gbr, few more util funcs

--- fish/config.fish
+++ fish/config.fish
@@ -25,11 +25,41 @@ function sep
     set -lx wreplace $argv[2]
     ag -s -l $wsearch | xargs sed -i '' s/$wsearch/$wreplace/g
 end
+function fid
+    lsof -n -i4TCP:$argv[1] | grep LISTEN
+end
+
 
 
 # --- Aliases ------------
 alias size    'du -sh'
 
 source /usr/local/share/autojump/autojump.fish
+status --is-interactive; and . (pyenv init -|psub)
+status --is-interactive; and . (pyenv virtualenv-init -|psub)
+pyenv activate myenv
 
-if test -f $HOME/.autojump/share/autojump/autojump.fish; . $HOME/.autojump/share/autojump/autojump.fish; end
+set -g fish_user_paths "/usr/local/opt/elasticsearch@5.6/bin" $fish_user_paths
+
+set fish_git_dirty_color red
+set fish_git_not_dirty_color green
+
+function parse_git_branch
+  set -l branch (git branch 2> /dev/null | grep -e '\* ' | sed 's/^..\(.*\)/\1/')
+  set -l git_status (git status -s)
+
+  if test -n "$git_status"
+    echo (set_color $fish_git_dirty_color)$branch(set_color normal)
+  else
+    echo (set_color $fish_git_not_dirty_color)$branch(set_color normal)
+  end
+end
+
+function fish_prompt
+  set -l git_dir (git rev-parse --git-dir 2> /dev/null)
+  if test -n "$git_dir"
+    printf '%s@%s %s%s%s:%s> ' (whoami) (hostname|cut -d . -f 1) (set_color $fish_color_cwd) (prompt_pwd) (set_color normal) (parse_git_branch)
+  else
+    printf '%s@%s %s%s%s> ' (whoami) (hostname|cut -d . -f 1) (set_color $fish_color_cwd) (prompt_pwd) (set_color normal)
+  end
+end
